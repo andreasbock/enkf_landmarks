a3012ec515bd7127608e0a6beb27f2a451552d67
Andreas Bock <bock@andreasbock.dk>
2020-06-21 15:22:25 +0200

    some renaming

--- test_lddmm.py
+++ test_lddmm.py
@@ -1,5 +1,5 @@
 import torch
-import lib_landmarks
+import utils
 from lddmm import shoot
 
 use_cuda = torch.cuda.is_available()
@@ -9,7 +9,7 @@ torchdeviceId = torch.device('cuda:0') if use_cuda else 'cpu'
 nt = 10
 nl = 20
 
-Q0_np, Q1_np, test_name = lib_landmarks.squeeze(nl)
+Q0_np, Q1_np, test_name = utils.squeeze(nl)
 
 Q0 = torch.tensor(Q0_np).float()
 Q1 = torch.tensor(Q1_np).float()
@@ -20,6 +20,6 @@ q1 = Q1.clone().detach().to(dtype=torchdtype, device=torchdeviceId).requires_gra
 sigma = torch.tensor([0.01], dtype=torchdtype, device=torchdeviceId)
 qs = shoot(q0, q1, nt, sigma)
 
-lib_landmarks.plot_q(qs)
+utils.plot_q(qs)
 
 
