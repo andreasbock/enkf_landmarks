809ad84548532d92b0ab6ef9f02da7ec3f52b39a
Andreas Bock <bock@andreasbock.dk>
2020-09-21 14:15:29 +0200

    shooting for targets

--- ensemble.py
+++ ensemble.py
@@ -12,7 +12,6 @@ torch_dtype = torch.float32
 class Ensemble:
     def __init__(self):
         self.ensemble = []
-        self.spatial_dimension = 2
 
     def size(self):
         return len(self.ensemble)
@@ -47,11 +46,16 @@ class Ensemble:
         for i in range(self.size()):
             self.ensemble[i] = op(self.ensemble[i], alpha[i])
 
+    def element_dimension(self):
+        if len(self.ensemble) == 0:
+            raise Exception("Cannot get dimension of Ensemble with no elements!")
+        return self.ensemble[0].size()
 
-def ensemble_normal(num_landmarks, ensemble_size):
+
+def ensemble_normal(num_landmarks, ensemble_size, scale=1):
     pe = Ensemble()
     for j in range(ensemble_size):
-        p0_np = utils.sample_normal(num_landmarks, 0, 1)
+        p0_np = scale * utils.sample_normal(num_landmarks, 0, 1)
         p0 = torch.tensor(p0_np, dtype=torch_dtype, requires_grad=True)
         pe.append(p0)
     return pe
@@ -60,7 +64,7 @@ def ensemble_normal(num_landmarks, ensemble_size):
 def target_from_momentum_ensemble(pe, template, time_steps=10, landmark_size=1):
     sigma = torch.tensor([landmark_size], dtype=torch_dtype)
     K = gauss_kernel(sigma=sigma)
-    target = torch.zeros((pe.size(), pe.spatial_dimension))
+    target = torch.zeros(pe.element_dimension())
     for p in pe.ensemble:
         # Example 2: Normal distribution
         target += lddmm_forward(p, template, K, time_steps)[-1][1]
--- generate_target.py
+++ generate_target.py
@@ -7,14 +7,14 @@ torch_dtype = torch.float32
 
 log_dir = f"TARGET_{utils.date_string()}/"
 ensemble_size = 10
-num_landmarks = 20
+num_landmarks = 15
 
 # 1) define template
 template_numpy = utils.circle(num_landmarks)
 template = torch.tensor(template_numpy, dtype=torch_dtype, requires_grad=True)
 
 # 2) define target by shooting with a random initial momenta
-pe = ensemble.ensemble_normal(num_landmarks, ensemble_size)
+pe = ensemble.ensemble_normal(num_landmarks, ensemble_size, scale=1.5)
 target = ensemble.target_from_momentum_ensemble(pe, template)
 
 # 3) Save the target, plot it and dump the initial momentum
