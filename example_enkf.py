516acd49de0a89d179b80d25ab323085358c8bd9
Andreas Bock <bock@andreasbock.dk>
2020-10-10 21:48:45 +0200

    split up computing & dumping results

--- example_enkf.py
+++ example_enkf.py
@@ -14,16 +14,14 @@ def run_enkf_on_target(data_dir, log_dir="./"):
     pe = MomentumEnsemble.load(data_dir + "/pe.pickle")
     target = utils.pload(data_dir + "/target.pickle")
 
-    # dump into log dir anyway
-    pe.save(log_dir + "p_initial.pickle")
-    utils.pdump(target, log_dir + "target.pickle")
-
     # 2) make a template to start from
     template_numpy = utils.circle(len(target))
     template = torch.tensor(template_numpy, dtype=torch_dtype, requires_grad=True)
-    utils.plot_landmarks(file_name=log_dir + "template_and_target",
-                         template=template,
-                         target=target)
+
+    # dump into log dir anyway
+    pe.save(log_dir + "p_initial.pickle")
+    utils.pdump(template, log_dir + "template.pickle")
+    utils.pdump(target, log_dir + "target.pickle")
 
     # 3) perturb ensemble by either
     #   A: adding noise:      p = p_target + \eta, \eta noise
@@ -38,13 +36,24 @@ def run_enkf_on_target(data_dir, log_dir="./"):
     # 4) set up and run Kalman filter
     ke = EnsembleKalmanFilter(template, target, log_dir=log_dir)
     p = ke.run(pe, target)
-    
-    # dump the results
+
+    # 5) dump or plot the results
     p.save(log_dir + "p_result.pickle")
 
-    # plot the results
+    return log_dir
+
+
+def dump_results(log_dir):
+
+    template = utils.pload(log_dir + "template.pickle")
+    target = utils.pload(log_dir + "target.pickle")
+
     utils.plot_errors(log_dir + "errors.pickle", log_dir + "errors.pdf")
-    utils.plot_consensus(log_dir + "consensus.pickle", log_dir + "errors.pdf")
+    utils.plot_consensus(log_dir + "consensus.pickle", log_dir + "consensus.pdf")
+
+    w = utils.pload(log_dir + "weight_vector.pickle")
+    with open(log_dir + "weight_vector_norm.pickle") as file:
+        file.write(f"Norm of w: {np.linalg.norm(w)}")
 
     target_pickles = glob.glob(log_dir + '/PREDICTED_TARGET_iter=*.pickle')
     for target_pickle in target_pickles:
@@ -54,8 +63,13 @@ def run_enkf_on_target(data_dir, log_dir="./"):
                              target=target,
                              file_name=file_name)
 
+    utils.plot_landmarks(file_name=log_dir + "template_and_target",
+                         template=template,
+                         target=target)
+
 
 if __name__ == "__main__":
     # run the EnKF on all the manufactured solutions in the `data` directory
     for target_data in glob.glob('./data/TARGET*'):
-        run_enkf_on_target(target_data)
+        log_dir = run_enkf_on_target(target_data)
+        dump_results(log_dir)
--- lddmm.py
+++ lddmm.py
@@ -1,17 +1,14 @@
-import time
 import numpy as np
 from torch.autograd import grad
 from pykeops.torch.kernel_product.formula import *
 import pykeops.torch as pk
 
-# torch type and device
 torch_dtype = torch.float32
 
 
 ####################################################################
 # Define standard Gaussian kernel
 # -------------------------------
-
 
 def gauss_kernel(sigma):
     def k(x, y, b):
@@ -29,7 +26,6 @@ def gauss_kernel(sigma):
 # ODE and Hamiltonian system
 # --------------------------
 
-
 def forward_euler():
     def f(ode_system, x0, nt):
         x = tuple(map(lambda _x: _x.clone(), x0))
@@ -40,14 +36,12 @@ def forward_euler():
             x = tuple(map(lambda _x, _xd: _x + _xd * dt, x, x_dot))
             xs.append(x)
         return xs
-
     return f
 
 
 def hamiltonian(k):
     def h(p, q):
         return .5 * (p * k(q, q, p)).sum()
-
     return h
 
 
@@ -69,6 +63,10 @@ def lddmm_forward(p0, q0, k, nt, integrator=forward_euler()):
     return integrator(hamiltonian_system(k), (p0, q0), nt)
 
 
+#####################################################################
+# Shooting
+# -------------------------------------------
+
 def lddmm_loss(k, nt, q1):
     def loss(p0, q0):
         p, q = lddmm_forward(p0, q0, k, nt)[-1]
@@ -76,11 +74,6 @@ def lddmm_loss(k, nt, q1):
         return functional
 
     return loss
-
-
-#####################################################################
-# Shooting
-# -------------------------------------------
 
 
 def shoot(q0, q1, nt, sigma, p0=None, epochs=15):
@@ -92,9 +85,6 @@ def shoot(q0, q1, nt, sigma, p0=None, epochs=15):
         p0 = torch.zeros(q0.shape, dtype=torch_dtype, requires_grad=True)
     optimizer = torch.optim.LBFGS([p0], max_eval=100)
 
-    print('Performing optimization...')
-    start = time.time()
-
     def closure():
         optimizer.zero_grad()
         _loss = loss(p0, q0)
@@ -102,12 +92,9 @@ def shoot(q0, q1, nt, sigma, p0=None, epochs=15):
         _loss.backward()
         return _loss
 
-    for i in range(epochs):
+    for _ in range(epochs):
         optimizer.step(closure)
         lddmm_forward(p0, q0, k, nt)
-
-    end = time.time()
-    print('Optimization took {:.2f}'.format(end - start))
 
     # final p/q solve
     pqs = lddmm_forward(p0, q0, k, nt)
