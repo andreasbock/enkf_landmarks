a469573b5d26c15cd4e34035e5958ba42c08faba
Andreas Bock <bock@andreasbock.dk>
2020-11-02 22:19:26 +0100

    added extension and some file name changes to generate_target.py

--- generate_target.py
+++ generate_target.py
@@ -14,11 +14,11 @@ template_numpy = utils.circle(num_landmarks)
 template = torch.tensor(template_numpy, dtype=torch_dtype, requires_grad=True)
 
 # 2) define target by shooting with a random initial momenta
-pe = ensemble.ensemble_normal(num_landmarks, ensemble_size, scale=1.5)
+pe = ensemble.ensemble_normal(num_landmarks, ensemble_size, scale=.65)
 target = ensemble.target_from_momentum_ensemble(pe, template)
 
 # 3) Save the target, plot it and dump the initial momentum
-pe.save(log_dir + "pe")
-utils.pdump(target, log_dir + "target")
-utils.plot_landmarks(file_name=log_dir + "target", template=template, target=target)
+pe.save(log_dir + "pe.pickle")
+utils.pdump(target, log_dir + "target.pickle")
+utils.plot_landmarks(file_name=log_dir + "template_and_target", template=template, target=target)
 
