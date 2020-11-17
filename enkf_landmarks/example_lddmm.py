76d469359077fa6b2002379f13ca6c7afd6b29e0
Andreas Bock <bock@andreasbock.dk>
2020-11-17 20:50:20 +0100

    compute time

--- enkf_landmarks/example_lddmm.py
+++ enkf_landmarks/example_lddmm.py
@@ -1,3 +1,4 @@
+import time
 import torch
 
 import enkf_landmarks.utils as utils
@@ -11,7 +12,12 @@ num_landmarks = 20
 template = torch.tensor(utils.circle(num_landmarks), dtype=torch_dtype, requires_grad=True)
 target = torch.tensor(utils.circle(num_landmarks, shift=2., scale=2.), dtype=torch_dtype, requires_grad=True)
 
+print("Shooting...")
+start = time.time()
 qs = shoot(template, target, time_steps)
+end = time.time()
+
+print(f"Done! Time elapsed: {end - start} seconds.")
 
 utils.plot_landmarks('example_lddmm',
                      qs=qs,
