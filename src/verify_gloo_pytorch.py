a8db662eb321a908574139b2726fa8da84878627
Andreas Bock <bock@andreasbock.dk>
2020-11-26 22:50:50 +0100

    Fix gloo script

--- src/verify_gloo_pytorch.py
+++ src/verify_gloo_pytorch.py
@@ -1,13 +1,27 @@
-"""run.py:"""
-#!/usr/bin/env python
 import os
+import time
 import torch
 import torch.distributed as dist
 from torch.multiprocessing import Process
 
+import src.utils as utils
+from src.lddmm import shoot
+
+torch_dtype = torch.float32
+
+time_steps = 10
+num_landmarks = 20
+
+template = torch.tensor(utils.circle(num_landmarks), dtype=torch_dtype, requires_grad=True)
+target = torch.tensor(utils.circle(num_landmarks, shift=2., scale=2.), dtype=torch_dtype, requires_grad=True)
+
+
 def run(rank, size):
-    """ Distributed function to be implemented later. """
-    pass
+    print(f"Rank: {rank} of {size}\n")
+    qs = shoot(template, target, time_steps)
+    utils.plot_landmarks(file_name=f'./Shooting={rank}', qs=qs, template=template, target=target)
+    return qs
+
 
 def init_process(rank, size, fn, backend='gloo'):
     """ Initialize the distributed environment. """
@@ -19,8 +33,11 @@ def init_process(rank, size, fn, backend='gloo'):
 
 if __name__ == "__main__":
     print("Remember to run 'export GLOO_SOCKET_IFNAME=en0'!")
-    size = 2
+
+    size = 4
     processes = []
+
+    starttime = time.time()
     for rank in range(size):
         p = Process(target=init_process, args=(rank, size, run))
         p.start()
@@ -28,3 +45,5 @@ if __name__ == "__main__":
 
     for p in processes:
         p.join()
+
+    print('That took {} seconds'.format(time.time() - starttime))
