07c3111980d40f7d5b27e7974cbb196a0a8be4bb
Andreas Bock <bock@andreasbock.dk>
2020-12-02 18:28:11 +0100

    artifact of remove  ensemble.py

--- src/manufacture_shape_data.py
+++ src/manufacture_shape_data.py
@@ -1,8 +1,8 @@
 import argparse
 import torch
 
-import src.ensemble as ensemble
 import src.utils as utils
+from src.lddmm import lddmm_forward, gauss_kernel
 
 torch_dtype = torch.float32
 
@@ -15,17 +15,35 @@ parser.add_argument('--std', type=float, default=.5, help='Momentum standard dev
 args = parser.parse_args()
 
 
+def ensemble_normal(num_landmarks, ensemble_size, mean=0, std=1):
+    pe = []
+    for j in range(ensemble_size):
+        p0 = utils.sample_normal(num_landmarks, mean, std)
+        pe.append(torch.tensor(p0, dtype=torch_dtype, requires_grad=True))
+    return pe
+
+
+def target_from_momentum_ensemble(pe, template, time_steps=10, landmark_size=1):
+    # TODO: make this parallel
+    sigma = torch.tensor([landmark_size], dtype=torch_dtype)
+    K = gauss_kernel(sigma=sigma)
+    target = torch.zeros(pe[0].size)
+    for p in pe:
+        target += lddmm_forward(p, template, K, time_steps)[-1][1]
+    return target / len(pe)
+
+
 def manufacture_from_normal_distribution(log_dir, ensemble_size, num_landmarks, mean, std):
     # 1) define template
     template_numpy = utils.circle(num_landmarks)
     template = torch.tensor(template_numpy, dtype=torch_dtype, requires_grad=True)
 
     # 2) define target by shooting with a random initial momenta
-    pe = ensemble.ensemble_normal(num_landmarks, ensemble_size, mean=mean, std=std)
-    target = ensemble.target_from_momentum_ensemble(pe, template)
+    pe = ensemble_normal(num_landmarks, ensemble_size, mean=mean, std=std)
+    target = target_from_momentum_ensemble(pe, template)
 
     # 3) Save the template, target and the initial ensemble & plot them
-    pe.save(log_dir + "pe.pickle")
+    utils.pdump(pe, log_dir + "pe.pickle")
     utils.pdump(template, log_dir + "template.pickle")
     utils.pdump(target, log_dir + "target.pickle")
     utils.plot_landmarks(file_name=log_dir + "template_and_target", template=template, target=target)
