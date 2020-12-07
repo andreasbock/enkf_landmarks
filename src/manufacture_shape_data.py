6872e5440a31c977b8d40e369848fd8b6a64fcd6
Andreas Bock <bock@andreasbock.dk>
2020-12-07 21:56:11 +0100

    creating shape data concurrently!

--- src/manufacture_shape_data.py
+++ src/manufacture_shape_data.py
@@ -1,60 +1,74 @@
 import argparse
 import torch
+import torch.distributed as dist
+from torch.multiprocessing import Process
 
 import src.utils as utils
+from src.enkf import _initialise_distributed_pytorch
 from src.lddmm import lddmm_forward, gauss_kernel
 
 torch_dtype = torch.float32
 
 
-def ensemble_normal(num_landmarks, ensemble_size, mean=0, std=1):
-    pe = []
-    for j in range(ensemble_size):
-        p0 = utils.sample_normal(num_landmarks, mean, std)
-        pe.append(torch.tensor(p0, dtype=torch_dtype, requires_grad=True))
-    return pe
+def make_normal_momentum(num_landmarks, mean, std):
+    _p = torch.distributions.normal.Normal(loc=mean, scale=std).sample([num_landmarks, 2])
+    return _p.clone().detach().requires_grad_(True).type(dtype=torch_dtype)
 
 
-def target_from_momentum_ensemble(pe, template, time_steps=10, landmark_size=1):
-    # TODO: make this parallel
-    sigma = torch.tensor([landmark_size], dtype=torch_dtype)
-    K = gauss_kernel(sigma=sigma)
-    target = torch.zeros(pe[0].size)
-    for p in pe:
-        target += lddmm_forward(p, template, K, time_steps)[-1][1]
-    return target / len(pe)
+def manufacture_from_normal_distribution(log_dir, ensemble_size, num_landmarks, mean, std, time_steps, landmark_size):
 
+    template = torch.tensor(utils.circle(num_landmarks), dtype=torch_dtype, requires_grad=True)
+    p_ensemble_result = [torch.zeros(size=(num_landmarks, 2)) for _ in range(ensemble_size)]
 
-def manufacture_from_normal_distribution(log_dir, ensemble_size, num_landmarks, mean, std):
-    # 1) define template
-    template_numpy = utils.circle(num_landmarks)
-    template = torch.tensor(template_numpy, dtype=torch_dtype, requires_grad=True)
+    def _spawn(rank, ranks, p):
+        _initialise_distributed_pytorch(rank, ranks)
+        dist.all_gather(p_ensemble_result, p)
 
-    # 2) define target by shooting with a random initial momenta
-    pe = ensemble_normal(num_landmarks, ensemble_size, mean=mean, std=std)
-    target = target_from_momentum_ensemble(pe, template)
+        # apply forward & reduce
+        sigma = torch.tensor([landmark_size], dtype=torch_dtype)
+        K = gauss_kernel(sigma=sigma)
+        target = lddmm_forward(p, template, K, time_steps)[-1][1]
+        dist.all_reduce(target)
+        target /= ranks
 
-    # 3) Save the template, target and the initial ensemble & plot them
-    utils.pdump(pe, log_dir + "pe.pickle")
-    utils.pdump(template, log_dir + "template.pickle")
-    utils.pdump(target, log_dir + "target.pickle")
-    utils.plot_landmarks(file_name=log_dir + "template_and_target", template=template, target=target)
+        if rank == 0:
+            utils.pdump(p_ensemble_result, log_dir + "pe.pickle")
+            utils.pdump(template, log_dir + "template.pickle")
+            utils.pdump(target, log_dir + "target.pickle")
+            utils.plot_landmarks(file_name=log_dir + "template_and_target", template=template, target=target)
 
-    return template, target, pe
+    processes = []
+    for rank in range(ensemble_size):
+        momentum = make_normal_momentum(num_landmarks, mean, std)
+        p = Process(target=_spawn, args=(rank, ensemble_size, momentum))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--log_dir', default=f"../TARGET_{utils.date_string()}/",
+    parser.add_argument('--log_dir', default=f"TARGET_{utils.date_string()}/",
                         help='Directory in which to dump data.')
-    parser.add_argument('--num_landmarks', type=int, default=50, help='Number of landmarks.')
+    parser.add_argument('--num_landmarks', type=int, default=150, help='Number of landmarks.')
     parser.add_argument('--ensemble_size', type=int, default=10, help='Number of ensemble elements.')
     parser.add_argument('--mean', type=float, default=0., help='Momentum mean.')
     parser.add_argument('--std', type=float, default=.5, help='Momentum standard deviation.')
+    parser.add_argument('--timesteps', type=int, default=10, help='Time steps for the forward map.')
+    parser.add_argument('--landmark_size', type=float, default=1, help='Landmark size.')
     args = parser.parse_args()
+    logger = utils.basic_logger(args.log_dir + '/manufactured_ensemble.log')
+
+    for key, value in args.__dict__.items():
+        logger.info(f"{key}: {value}")
+        utils.pdump(value, args.log_dir + f"/{key}.pickle")
 
     manufacture_from_normal_distribution(log_dir=args.log_dir,
                                          ensemble_size=args.ensemble_size,
                                          num_landmarks=args.num_landmarks,
                                          mean=args.mean,
-                                         std=args.std)
+                                         std=args.std,
+                                         time_steps=args.timesteps,
+                                         landmark_size=args.landmark_size)
