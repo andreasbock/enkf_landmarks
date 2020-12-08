8e8aa0b5eadf038d681018532ed860b228c0321d
Andreas Bock <bock@andreasbock.dk>
2020-12-08 18:43:08 +0100

    small fixes h & t

--- src/manufacture_shape_data.py
+++ src/manufacture_shape_data.py
@@ -32,7 +32,7 @@ def manufacture_from_normal_distribution(log_dir, ensemble_size, num_landmarks, 
         target /= ranks
 
         if rank == 0:
-            utils.pdump(p_ensemble_result, log_dir + "pe.pickle")
+            utils.pdump(p_ensemble_result, log_dir + "p_ensemble_truth.pickle")
             utils.pdump(template, log_dir + "template.pickle")
             utils.pdump(target, log_dir + "target.pickle")
             utils.plot_landmarks(file_name=log_dir + "template_and_target", template=template, target=target)
@@ -49,14 +49,13 @@ def manufacture_from_normal_distribution(log_dir, ensemble_size, num_landmarks, 
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--log_dir', default=f"TARGET_{utils.date_string()}/",
-                        help='Directory in which to dump data.')
+    parser = argparse.ArgumentParser(description='Manufacture some random landmark configurations.')
+    parser.add_argument('--log_dir', default=f"TARGET_{utils.date_string()}/", help='Directory in which to dump data.')
     parser.add_argument('--num_landmarks', type=int, default=150, help='Number of landmarks.')
     parser.add_argument('--ensemble_size', type=int, default=10, help='Number of ensemble elements.')
     parser.add_argument('--mean', type=float, default=0., help='Momentum mean.')
-    parser.add_argument('--std', type=float, default=.5, help='Momentum standard deviation.')
-    parser.add_argument('--timesteps', type=int, default=10, help='Time steps for the forward map.')
+    parser.add_argument('--std', type=float, default=1., help='Momentum standard deviation.')
+    parser.add_argument('--timesteps', type=int, default=1, help='Time steps for the forward map.')
     parser.add_argument('--landmark_size', type=float, default=1, help='Landmark size.')
     args = parser.parse_args()
     logger = utils.basic_logger(args.log_dir + '/manufactured_ensemble.log')
