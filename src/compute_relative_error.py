27d9c9db447379735c1a925996dfee29c06f9757
Andreas Bock <bock@andreasbock.dk>
2021-06-14 22:25:52 +0200

    added table of relative error at last iteration

--- src/compute_relative_error.py
+++ src/compute_relative_error.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 import numpy as np
+import csv
 import re
 from pathlib import Path
 
@@ -16,29 +18,41 @@ def compute_relative_error(truth):
     return _compute_relative_error
 
 
+def iter_sort(str: Path):
+    return int(re.search('[0-9]+', str.name).group())
+
+
+def default_dict():
+    return defaultdict(default_dict)
+
+
 if __name__ == "__main__":
+    results = default_dict()
     # plot the misfits for each target/landmark/ensemble size together
     for path in Path('RESULTS_ENKF/').glob('LANDMARKS=*/TARGET*/ENSEMBLE_SIZE=*'):
         path_string = str(path)
 
-        # fix this:
         landmarks = re.search('LANDMARKS=([0-9]+)', path_string).group()
         ensemble_size = re.search('ENSEMBLE_SIZE=([0-9]+)', path_string).group()
-        target = re.search('TARGET_(.)*/', path_string).group()
+        target = re.search('TARGET_(.)*/', path_string).group()[:-1]
         truth_base = Path(f'data/{landmarks}/{target}')
         truth = utils.pload(str(truth_base / 'p_truth.pickle'))
+        _relative_error = compute_relative_error(truth)
 
         # plot relative error for each realisation
         plt.figure()
-        _relative_error = compute_relative_error(truth)
+        sum_relative_error = 0.
+        num_realisations = 0
         for realisation in path.glob('REALISATION*'):
             p_means = []
-            for p_mean_path in realisation.glob('P_mean_iter=*.pickle'):
+            for p_mean_path in sorted(realisation.glob('P_mean_iter=*.pickle'), key=iter_sort):
                 p_mean = utils.pload(str(p_mean_path))
                 p_means.append(p_mean)
 
             relative_errors = list(map(_relative_error, p_means))
             plt.loglog(range(1, len(relative_errors) + 1), relative_errors)
+            sum_relative_error += relative_errors[-1]
+            num_realisations += 1
 
         plt.xlabel(r'Iteration $k$', fontsize=20)
         plt.ylabel(r'Relative error', fontsize=20)
@@ -46,3 +60,17 @@ if __name__ == "__main__":
         destination = str(path / f'../relative_error_{landmarks}_{ensemble_size}.pdf')
         plt.savefig(destination, bbox_inches='tight')
         plt.close()
+
+        # write csv
+        results[landmarks][target][ensemble_size] = sum_relative_error / num_realisations
+
+    csv_path = Path('RESULTS_ENKF') / 'mean_relative_errors_{landmarks}.csv'
+    header = ['10', '50', '100']  # ensemble size
+    for landmark, data in results.items():
+        with open(str(csv_path).format(landmarks=landmark), 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(header)
+            for target, ensemble_sizes in data.items():
+                sorted_es = sorted(ensemble_sizes, key=lambda x: int(re.search('[0-9]+', x).group()))
+                writer.writerow([ensemble_sizes[es] for es in sorted_es])
+
